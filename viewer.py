9609b47a456a1eed04d7180467036e658e1c7260
Alex Chan <alex@alexwlchan.net>
2018-05-26 23:22:13 +0100

    Okay, now we actually create indices

--- viewer.py
+++ viewer.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8
 
+from elasticsearch import Elasticsearch
 from flask import Flask
 
 app = Flask(__name__)
 
+client = Elasticsearch(hosts=['http://localhost:9200'])
+
+from taggle.elastic import create_index
+
 
 @app.route('/')
 def index():
+    create_index(client=client, name='taggle-test-index')
     return 'Hello world!'
 
 
