000fb6310f4bc0dbc046bb06add69ba6d3f19bf3
Alex Chan <alex@alexwlchan.net>
2018-05-27 12:26:29 +0100

    Give a way to get all bookmark metadata

--- examples/pinboard/pinboard.py
+++ examples/pinboard/pinboard.py
@@ -1,6 +1,9 @@
 # -*- encoding: utf-8
 
 import datetime as dt
+import json
+import os
+import tempfile
 
 import requests
 
@@ -11,6 +14,8 @@ class PinboardManager:
         self.username = username
         self.password = password
         self.last_fetched = None
+
+        self.cache_dir = tempfile.mkdtemp()
 
     def __repr__(self):
         return '<%s username=%r>' % (type(self).__name__, self.username)
@@ -28,7 +33,10 @@ class PinboardManager:
         resp.raise_for_status()
         return resp.json()
 
-    def update(self):
+    def cache_path(self, path):
+        return os.path.join(self.cache_dir, path)
+
+    def get_bookmark_metadata(self):
         last_updated_resp = self._api_get('/posts/update')['update_time']
         last_updated_date = dt.datetime.strptime(
             last_updated_resp, '%Y-%m-%dT%H:%M:%SZ')
@@ -38,7 +46,13 @@ class PinboardManager:
             (last_updated_date - self.last_fetched).seconds > 60
         ):
             print('We need to do an update!')
+            self.last_fetched = last_updated_date
+
+            posts = self._api_get('/posts/all')
+            json_str = json.dumps(posts)
+            with open(self.cache_path('bookmarks.json'), 'w') as f:
+                f.write(json_str)
+            return posts
         else:
             print("We're already up-to-date!")
-
-        self.last_fetched = last_updated_date
+            return json.load(open(self.cache_path('bookmarks.json')))
